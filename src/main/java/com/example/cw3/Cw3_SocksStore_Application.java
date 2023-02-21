493a9bebd90b73016246a790bfef98e8aaae833c
denispetko <den.petko@rambler.ru>
2023-02-21 21:21:30 +0300

    init

---
+++ src/main/java/com/example/cw3/Cw3_SocksStore_Application.java
@@ -0,0 +1,13 @@
+package com.example.cw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw3_SocksStore_Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cw3_SocksStore_Application.class, args);
+	}
+
+}
