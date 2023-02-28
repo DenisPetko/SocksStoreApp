e88d798fda944cf68d6f35431da12bd8ec651130
denispetko <den.petko@rambler.ru>
2023-02-28 09:57:10 +0300

    init

--- src/main/java/com/example/cw3/controller/FileController.java
+++ src/main/java/com/example/cw3/controller/FileController.java
@@ -8,10 +8,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -44,7 +41,7 @@ public class FileController {
         }
     }
 
-    @GetMapping(value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "Загрузить файл")
     public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file) {
         try {
